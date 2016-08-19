4cd30bef7d1bd6353f141e7a893e5635519049ef
Ian Keen <iankeen82@gmail.com>
2016-08-19 12:52:00 -0700

    update dependencies

--- Package.swift
+++ Package.swift
@@ -9,8 +9,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/qutheory/engine.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/qutheory/vapor-tls", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/vapor/tls-provider.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1)
     ],
     exclude: [
