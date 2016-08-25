e5733a5bfe8612e3440fc212fb3b865e27aadbb1
Ian Keen <iankeen82@gmail.com>
2016-08-24 17:50:38 -0700

    added Redis Storage

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/tls-provider.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 9)
     ],
     exclude: [
         "XcodeProject"
