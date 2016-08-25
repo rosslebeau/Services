002760f56c8863b9607c07bb9d21437c87c8d9e5
Ian Keen <iankeen82@gmail.com>
2016-08-24 21:26:42 -0700

    removes Redis :(

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/tls-provider.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 9)
+        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1)
     ],
     exclude: [
         "XcodeProject"
