3afc31b0c08ba2a2f7e0141db1571a5486a67476
Ian Keen <iankeen82@gmail.com>
2016-08-24 23:01:42 -0700

    added forked redis dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/tls-provider.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/ChameleonBot/Common.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/IanKeen/Redbird.git", majorVersion: 1, minor: 0),
     ],
     exclude: [
         "XcodeProject"
