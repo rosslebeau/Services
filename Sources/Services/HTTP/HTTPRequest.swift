8e50b7e5b5c84ee7758a97025f69d25d39168837
Ian Keen <iankeen82@gmail.com>
2016-09-14 17:16:35 -0700

    relaxed permissions on HTTPRequest

--- Sources/Services/HTTP/HTTPRequest.swift
+++ Sources/Services/HTTP/HTTPRequest.swift
@@ -2,12 +2,12 @@ import Foundation
 
 /// Represents a HTTP Request
 public struct HTTPRequest {
-    public let method: HTTPRequestMethod
-    public let url: URL
-    public let port: Int
-    public let parameters: [String: String]?
-    public internal(set) var headers: [String: String]?
-    public let body: [String: Any]?
+    public var method: HTTPRequestMethod
+    public var url: URL
+    public var port: Int
+    public var parameters: [String: String]?
+    public var headers: [String: String]?
+    public var body: [String: Any]?
     
     public init(method: HTTPRequestMethod, url: URL, port: Int = 443, parameters: [String: String]? = nil, headers: [String: String]? = nil, body: [String: Any]? = nil) {
         self.method = method
